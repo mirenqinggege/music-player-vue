7fb33a40ed4bc13ae32a1b24200c9da95d555dce
zkh <1650697374@qq.com>
2024-11-14 17:14:11 +0800

    更新版本

--- src/api/recommend.ts
+++ src/api/recommend.ts
@@ -1,32 +1,50 @@
 import request from '@/api/request'
-import {Banner, Response, SongList} from '@/types'
+import {Banner, RecommendReason, Response, SongList, Track} from '@/types'
 
 interface BannerListResponse extends Response {
-  banners: Banner[]
+    banners: Banner[]
 }
 
 // 获取首页banner
 export function getBanner(): Promise<BannerListResponse> {
-  return request(
-    'POST',
-    'https://music.163.com/api/v2/banner/get',
-    {clientType: 'pc'},
-    {crypto: 'api'}
-  )
+    return request(
+        'POST',
+        'https://music.163.com/api/v2/banner/get',
+        {clientType: 'pc'},
+        {crypto: 'api'}
+    )
 }
 
 interface RecommendSongListResponse extends Response {
-  featureFirst: boolean
-  haveRcmdSongs: boolean
-  recommend: SongList[]
+    featureFirst: boolean
+    haveRcmdSongs: boolean
+    recommend: SongList[]
+}
+
+interface RecommendSongResponse extends Response {
+    data: {
+        dailySongs: Array<Track>,
+        orderSongs: Array<Track>,
+        recommendReasons: Array<RecommendReason>
+    }
 }
 
 // 获取每日推荐歌单
 export function getRecommendSongList(): Promise<RecommendSongListResponse> {
-  return request(
-    'POST',
-    'https://music.163.com/weapi/v1/discovery/recommend/resource',
-    {},
-    {crypto: 'weapi'}
-  )
+    return request(
+        'POST',
+        'https://music.163.com/weapi/v1/discovery/recommend/resource',
+        {},
+        {crypto: 'weapi'}
+    )
 }
+
+export function getRecommendSong(): Promise<RecommendSongResponse> {
+    return request(
+        'POST',
+        "https://music.163.com/api/v3/discovery/recommend/songs",
+        {},
+        {crypto: 'weapi'}
+    )
+}
+
